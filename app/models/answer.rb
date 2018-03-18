210c856403bc4aaae3b75934556a88771c37a60e
shivi <srpurwaha201@gmail.com>
2018-03-18 14:57:30 +0530

    deoendent destroy comments and answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   acts_as_votable
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :user
-  has_many :answers
+  has_many :answers, :dependent => :destroy
   acts_as_votable
 end
